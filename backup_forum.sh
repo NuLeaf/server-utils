e00644aed6b309b224afbe141262f344777c4198
Wolfgang C. Strack <wolfgangcstrack@gmail.com>
2016-03-03 18:14:57 -0800

    Tweak backup utils for use on server

--- backup_forum.sh
+++ backup_forum.sh
@@ -2,12 +2,12 @@
 # Author: Wolfgang C. Strack
 # Note: Run as sudo plox
 
-log_file='./backup_forum.log'
+log_file='/home/nuleafte/server_util/backup_forum.log'
 timestamp='date +%F_%H%M%S' # save command for multiple uses
 status=0
 
 folder_to_backup='/var/www/html/forum'
-file_to_write_to="~/website_backups/FORUM-BACKUP_$(date +%F_%H%M%S).tar.gz"
+file_to_write_to="/home/nuleafte/website_backups/FORUM-BACKUP_$(date +%F_%H%M%S).tar.gz"
 
 read -p "Back up forum files? [Y/N]: " choice
 if [[ "$choice" =~ [yY]([eE][sS])*?$ ]]; then
--- backup_main.sh
+++ backup_main.sh
@@ -2,12 +2,12 @@
 # Author: Wolfgang C. Strack
 # Note: Run as sudo plox
 
-log_file='./backup_main.log'
+log_file='/home/nuleafte/server_util/backup_main.log'
 timestamp='date +%F_%H%M%S' # save command for multiple uses
 status=0
 
 folder_to_backup='/var/www/html/ --exclude /var/www/html/forum'
-file_to_write_to="~/website_backups/MAIN-BACKUP_$(date +%F_%H%M%S).tar.gz"
+file_to_write_to="/home/nuleafte/website_backups/MAIN-BACKUP_$(date +%F_%H%M%S).tar.gz"
 
 read -p "Back up main website files? [Y/N]: " choice
 if [[ "$choice" =~ [yY]([eE][sS])*?$ ]]; then
